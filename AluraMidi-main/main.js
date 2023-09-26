8b7dd2ceb15b1ec37660e2753b8eafc09d5faed0
biancaspada15 <126569342+biancaspada15@users.noreply.github.com>
2023-09-26 08:25:17 -0300

    Update main.js

--- AluraMidi-main/main.js
+++ AluraMidi-main/main.js
@@ -1,1 +1,19 @@
 document.querySelector('.tecla_pom')
+document.querySelector('#som_tecla_pom')
+<audio id="som_tecla_pom" src="sounds/keyq.wav">
+
+document.querySelector('#som_tecla_pom').play
+function play()
+
+document.querySelector('#som_tecla_pom').play()
+Promise { <state>: "fulfilled", <value>: undefined }
+
+document.querySelector('#som_tecla_pom').play()
+Promise { <state>: "pending" }
+
+document.querySelector('#som_tecla_pom').play()
+Promise { <state>: "pending" }
+
+​
+
+
