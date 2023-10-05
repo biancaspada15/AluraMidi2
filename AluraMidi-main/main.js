c84a8a0aa78c2fa1aff06b0d82d83f5f3f5beb3a
biancaspada15 <126569342+biancaspada15@users.noreply.github.com>
2023-10-05 11:12:41 -0300

    Update main.js

--- AluraMidi-main/main.js
+++ AluraMidi-main/main.js
@@ -1,6 +1,12 @@
 function tocaSomPom(){
-document.querySelector('.tecla_pom').onclick= tocaSomPom;
-  document.querySelector('.#som_tecla_pom'). play();
+ document.querySelector('.#som_tecla_pom'). play();
+  document.querySelector('.tecla_pom').onclick= tocaSomPom;
+ function tocaSomclap(){
+ document.querySelector('.#som_tecla_clap'). play();
+  document.querySelector('.tecla_clap').onclick= tocaSomPom;
+   function tocaSomPom(){
+ document.querySelector('.#som_tecla_pom'). play();
+  document.querySelector('.tecla_pom').onclick= tocaSomPom;
 }
 
 ​
